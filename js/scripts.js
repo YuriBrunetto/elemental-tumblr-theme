6ece6f15b9f68541f5a57b58190cbcd125be8bda
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-04-12 21:52:13 -0300

    add search

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    // hamburger menu
     var $this, overlay;
 
     $("#menu").click(function(){
@@ -10,7 +11,6 @@ $(function(){
             $(".header-social").show().addClass("animated bounceInLeft");
             $(".overlay").addClass("animated fadeIn");
             $(".header-social").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-
                 $this.show();
             });
         }, 200);
@@ -32,5 +32,42 @@ $(function(){
             }
         }
     });
+
+    // search
+    $("#search").click(function(){
+        overlay = "<div class='overlay'></div>";
+        $("body").append(overlay);
+        $(".overlay").addClass("animated fadeIn");
+        $(".search-wrap").css("display", "flex").fadeIn(500);
+        setTimeout(function(){
+            $(".search-input").addClass("is-active");
+        }, 300);
+    });
+
+    $(".search-close").click(function(){
+        $(".search-input").removeClass("is-active");
+        setTimeout(function(){
+            $(".overlay").removeClass("fadeIn").addClass("fadeOut");
+            $(".overlay").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                $(".overlay").remove();
+            });
+            $(".search-wrap").fadeOut(500).delay(500).hide();
+        }, 300);
+    });
+
+    var hide_search = (function(){
+        if ($(".overlay").hasClass("fadeIn")) {
+            $(".search-input").removeClass("is-active");
+            setTimeout(function(){
+                $(".overlay").removeClass("fadeIn").addClass("fadeOut");
+                $(".overlay").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                    $(".overlay").remove();
+                });
+                $(".search-wrap").fadeOut(500).delay(500).hide();
+            }, 300);
+        }
+    });
+
+    $(window).resize(function(){ hide_search(); });
 
 });
